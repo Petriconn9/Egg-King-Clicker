ecb6927614e5fb984981fef3a0c4ef90b3776219
Petriconn9 <vicente.darwin52@gmail.com>
2026-06-25 18:27:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btn_musica = document.getElementById("boton musica")
 const musica = document.getElementById("musica")
+const contador = document.getElementById("dinero")
+const btn_click = document.getElementById("Boton")
 
 btn_musica.onclick = function() {
     if (musica.paused) {
@@ -7,4 +9,11 @@ btn_musica.onclick = function() {
     } else {
         musica.pause()
     }
+}
+
+let Dinero = 0
+btn_click.onclick = function() {
+    Dinero += 1;
+    console.log(Dinero);
+    contador.innerHTML = "Dinero: " + Dinero;
 }
