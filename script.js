56127e6f5e28f9cbce87445bc8e3b503e546c589
Petriconn9 <vicente.darwin52@gmail.com>
2026-06-25 17:59:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const btn_musica = document.getElementById("boton musica")
 const musica = document.getElementById("musica")
 
-btn_musica.onclick function() {
-    musica.onplay();
+btn_musica.onclick = function() {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
 }
